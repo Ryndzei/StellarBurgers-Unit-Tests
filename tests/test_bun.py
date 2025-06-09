0e738cdd496ac90037227c917f2e01aeacae88b9
Anton Rineyskiy <arineyskiy@yandex.ru>
2025-06-09 20:36:16 +0300

    Bun test changed

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -4,13 +4,14 @@ from praktikum.bun import Bun
 
 class TestBun:
 
-    @pytest.mark.parametrize("name, price", [
-        ("black bun", 100),
-        ("white bun", 200),
-        ("red bun", 300)
-    ])
-    def test_buns_get_name_and_price_successfully(self, name, price):
-        bun = Bun(name, price)
+    @pytest.mark.parametrize("name", ["black bun", "white bun", "red bun"])
+    def test_buns_get_name_successfully(self, name):
+        bun = Bun(name, 100)
 
         assert bun.get_name() == name
+
+    @pytest.mark.parametrize("price", [100, 200, 300])
+    def test_buns_get_price_successfully(self, price):
+        bun = Bun("black bun", price)
+
         assert bun.get_price() == price
